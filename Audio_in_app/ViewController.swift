c171c5ca57a5a1f013fcc7c327a76f360a4cc12d
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-07-10 12:32:59 +0200

    Audio_play_app

--- Audio_in_app/ViewController.swift
+++ Audio_in_app/ViewController.swift
@@ -7,12 +7,73 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
+    
+    
+    
+    
+    
 
+    @IBAction func playButton(_ sender: Any) {
+        
+         player.play()
+
+    }
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        
+        if player.isPlaying {
+            
+            player.pause()
+        }
+        
+    }
+    
+    @IBAction func restartButton(_ sender: Any) {
+        
+        
+        if player.isPlaying {
+            
+            player.currentTime = 0
+            player.play()
+            
+        }
+        
+    }
+    
+    @IBAction func slider(_ sender: Any) {
+        
+        player.volume = sliderOutlet.value
+       
+    
+    }
+    
+    @IBOutlet weak var sliderOutlet: UISlider!
+
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //let audioPath = Bundle.main.path(forResource: "Bach", ofType: "mp3")
+        
+        do {
+            
+           player = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Bach", ofType: "mp3")!))
+    
+            
+        } catch {
+            
+            print(error)
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
